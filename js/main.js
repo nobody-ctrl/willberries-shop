11fd1ec4ed3405806007afcf7536b83eb88b3083
nobody-ctrl <anastasiia.petrova@protonmail.com>
2021-03-25 22:42:27 +0100

    create: menu cart

--- js/main.js
+++ js/main.js
@@ -8,3 +8,36 @@ const mySwiper = new Swiper('.swiper-container', {
 	},
 });
 
+//card
+const buttonCard = document.querySelector('.button-cart');
+const modalCard = document.querySelector('#modal-cart');
+const modalClose = document.querySelector('.modal-close');
+const overlay = document.querySelector('.overlay');
+
+const openAndCloseModal = function(){
+	modalCard.classList.toggle('show');
+}
+buttonCard.addEventListener('click', openAndCloseModal);
+modalClose.addEventListener('click', openAndCloseModal);
+overlay.addEventListener('click', function(event){
+	if (event.target.getAttribute('id') == "modal-cart"){
+		openAndCloseModal();
+	}
+});
+
+//scroll smooth
+
+const scrollLink = document.querySelectorAll('a.scroll-link');
+
+for (let i = 0; i < scrollLink.length; i++){
+	scrollLink[i].addEventListener('click', function(event){
+		event.preventDefault();
+		const id = scrollLink[i].getAttribute('href');
+		document.querySelector(id).scrollIntoView({
+			behavior: "smooth",
+			block: "start"
+		});
+	});
+}
+
+
